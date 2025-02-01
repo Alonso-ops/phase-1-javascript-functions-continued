69f34b4d19f07e32c786d3fee81a3913227c7ecc
Fabian A Ortiz Chavez <alonsofabianortiz@icloud.com>
2025-02-01 00:03:30 -0700

    continued lab

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
-// code your solution here
+function saturdayFun(team = "play, soccer"){
+    if (`This Saturday, I want to ${team}!`);
+}
+saturdayFun()
+function saturdayFun(activity = "roller-skate") {
+    return `This Saturday, I want to ${activity}!`
+}
+ const mondayWork = function(activity = "go to the office") {
+ return `This Monday, I will ${activity}.`;
+    
+}
+console.log(mondayWork())
+console.log(mondayWork("work from home."))
+
+function wrapAdjective(wrapper = "*") {
+    return function(adjective = "special") {
+        return `You are ${wrapper}${adjective}${wrapper}!`;
+    };
+}
